a5465a5c6f07de20e6936d21ae4096de51f1a3f1
Abdur Rahaman <54670007+AbdurRahamanWP@users.noreply.github.com>
2024-09-29 23:14:25 +0600

    updated

--- includes/Admin/new_project.php
+++ includes/Admin/new_project.php
@@ -2,11 +2,14 @@
 	        <?php 
 		    	if( isset( $_GET['inserted'] )){ ?>
 			<h4 class='successfull'>Data Updated Successfully</h4>
-			<?php } ?>
+			<?php }
+             $settings = json_decode(get_option('scroll_to_top'),true);
+             
+            ?>
             
 			<h2> Scroll To Top Settings</h2>
-            <form class="scroll_top_form" id="scroll_top_form" action="options.php" method="post">
-                <!-- <div class="form-group row">
+            <form class="scroll_top_form" id="scroll_top_form" action="" method="post">
+                <div class="form-group row">
                     <label for="inputEmail3" class="col-sm-4 col-form-label">Select Type</label>
                     <div class="col-sm-6">
                         <select class="form-control" id="scroll_top_type" name='scroll_top_type'>
@@ -16,12 +19,11 @@
                             <option value='pill'>Pill Format</option>
                         </select>
                     </div>
-                </div> -->
-            
+                </div> 
                 <div class="form-group row">
                     <label for="inputEmail3" class="col-sm-4 col-form-label">Button Height</label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" id="button_height" name="button_height" value="<?php print get_option('button_height'); ?>" placeholder="Enter Button Height. Ex - 10px">
+                        <input type="text" class="form-control" id="button_height" name="button_height" value="<?php echo $settings['height']; ?>" placeholder="Enter Button Height. Ex - 10px">
                     </div>
                 </div>
                 <div class="form-group row">
@@ -77,17 +79,31 @@
                 <div class="form-group row">
                     <div class="col-sm-8">
                     <?php wp_nonce_field('update-options'); ?>
-                    <input type="hidden" name="action" value="update">
-                    <input type="hidden" name="page_options" value="button_height">
-                    <input type="hidden" name="page_options" value="button_width">
-                    <input type="hidden" name="page_options" value="padding_right">
-                    <input type="hidden" name="page_options" value="padding_buttom">
-                    <input type="hidden" name="page_options" value="border_Radius">
-                    <input type="hidden" name="page_options" value="background_color">
-                    <input type="hidden" name="page_options" value="button_icon_img">
-                    <input type="hidden" name="page_options" value="text_color">
                     <button type="submit" name="submit" id='submit' class="btn btn-primary">Save Information</button>
                     </div>
                 </div>
             </form>
+
+
+            <?php
+        
+            if(!isset($_POST['submit'])){
+                return;
+            }  
+
+            $data = [
+                'scroll_top_type'  => isset($_POST['scroll_top_type']) ? $_POST['scroll_top_type'] : '',
+                'height' => isset($_POST['button_height']) ? $_POST['button_height'] : '',
+                'button_width' => isset($_POST['button_width']) ? $_POST['button_width'] : '',
+                'padding_right' => isset($_POST['padding_right']) ? $_POST['padding_right'] : '',
+                'padding_buttom' => isset($_POST['padding_buttom']) ? $_POST['padding_buttom'] : '',
+                'border_Radius' => isset($_POST['border_Radius']) ? $_POST['border_Radius'] : '',
+                'background_color' => isset($_POST['background_color']) ? $_POST['background_color'] : '',
+                'button_icon_img' => isset($_POST['button_icon_img']) ? $_POST['button_icon_img'] : '',
+                'text_color' => isset($_POST['text_color']) ? $_POST['text_color'] : ''
+            ];
+
+            update_option('scroll_to_top', json_encode($data));
+            
+            ?>
 </div>
